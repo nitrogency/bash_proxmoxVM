5e2419935752087a306fcfa395bcf8a3b99a447a
nitrogency <no-antenna@tutanota.com>
2024-10-15 09:43:30 +0300

    hard drive mounting, additional options

--- host_setup.sh
+++ host_setup.sh
@@ -1,33 +1,36 @@
 #!/bin/bash
 
-OPTSTRING=":p:n:a:i:"
-
 MEMORY=2048
 CORE=2
-DEST=local
-DISK_SIZE=+45G
-DISK_NAME=vm-$ID-disk-0
+DEST=local-lvm
+DISK_SIZE=30
+DISK_NAME=vmdisk
 DISK_ID=$DEST:$DISK_NAME
 DISK_DEVICE=scsi0
 START=True
 HOSTBOOT=True
 IP=""
 GATEWAY=""
+NAMESERVER=""
 
 usage() {
-  echo "Usage: $0 [-p /path/to/image] [-n name] [-a ip] [-i id]"
-  echo "Options (required):"
-  echo "  -p(--path): path to image"
-  echo "  -n(--name): name of the VM you want to create"
-  echo "  -i(--id): ID of the VM you want to create"
-  echo "Options (optional):"
-  echo "  -c(--cpu): number of CPU cores (default - 2)"
-  echo "  -a(--ip): IPv4 address of the VM you want to create in CIDR notation (default - auto)"
-  echo "  -g(--gateway): gateway IPv4 address of the VM you want to create (default - auto)"
-  echo "  -m(--memory): amount of RAM in MB (default - 2048)"
-  echo "  -s(--nostart): does not start the VM after creation (default - false)"
-  echo "  -b(--noboot): does not automatically start the VM when the host system boots (default - false)"
-  exit 1
+    echo "Usage: $0 [-p /path/to/image] [-n name] [-a ip] [-i id]"
+    echo "Options (required):"
+    echo "  -p(--path): path to image."
+    echo "  -n(--name): name of the VM you want to create."
+    echo "  -i(--id): ID of the VM you want to create."
+    echo "Options (optional):"
+    echo "  -c(--cpu): number of CPU cores. (default - 2)"
+    echo "  -a(--ip): IPv4 address of the VM you want to create in CIDR notation. NOTE: setting the gateway is required if using this option. (default - DHCP)"
+    echo "  -g(--gateway): gateway IPv4 address of the VM you want to create. NOTE: setting the IP address is required if using this option. (default - DHCP)"
+    echo "  -m(--memory): amount of RAM in MB. (default - 2048)"
+    echo "  -s(--nostart): does not start the VM after creation. (default - false)"
+    echo "  -b(--noboot): does not automatically start the VM when the host system boots. (default - false)"
+    echo "  -h(--help): display this help and exit."
+    echo "  -d(--disksize): disk size in GB. (default - 30G)"
+    echo "  -ns(--nameserver): IP address of the nameserver. (default - uses host's)"
+    echo "  -dev(--networkdev): specify network devices to include."
+    exit 1
 }
 
 required=("ID" "NAME" "IMAGE")
@@ -81,6 +84,11 @@ menu() {
                 IP=$2
                 shift 2
                 ;;
+            -ns | --nameserver)
+                check_arg $1 $2
+                NAMESERVER=$2
+                shift 2
+                ;;
             -g | --gateway)
                 check_arg $1 $2
                 GATEWAY=$2
@@ -118,18 +126,61 @@ create_vm() {
     ISO_NAME=$(echo "$IMAGE" | awk -F/ '{print $NF}')
 
     qm create $ID --memory $MEMORY --core $CORE --name $NAME 
-    if [ "$START" == "True" ]; then
-        qm start $ID
-    fi
+    echo "VM created:"
+    echo "IMAGE: $IMAGE"
+    echo "ID: $ID"
+    echo "NAME: $NAME"
+    echo "CPU: $CORE"
+    echo "MEMORY: $MEMORY"
+
     if [ "$HOSTBOOT" == "True" ]; then
         qm set $ID -onboot 1
+        echo "HOSTBOOT: True - VM will automatically start with host boot"
     fi
     if [ "$IP" != "" ] && [ "$GATEWAY" != "" ]; then
         qm set $ID -ipconfig0 ip=$IP,gw=$GATEWAY
+        echo "IP: $IP - GATEWAY: $GATEWAY"
     fi
+    if [ "$NAMESERVER" != "" ]; then
+        qm set $ID --nameserver $NAMESERVER
+        echo "NAMESERVER: $NAMESERVER"
+    fi
+
+    qm set $ID --scsi0 $DEST:${DISK_SIZE}
+    qm set $ID -efidisk0 $DEST:${DISK_SIZE},efitype=4m
+    qm set $ID -machine q35
+    qm set $ID --agent enabled=1
     
     qm set $ID --ide2 local:iso/$ISO_NAME,media=cdrom
     qm set $ID --boot order=ide2
+    echo "ISO successfully mounted"
+
+    # UEFI, machine -> q35, QEMU agent, bridge/network device
+
+    if [ "$START" == "True" ]; then
+        qm start $ID
+        echo "Starting VM..."
+    else
+        echo "NOTE: You have disabled automatic startup after install. 
+        This may result in you having to manually swap the boot order."
+    fi
+
+    # Automatically switches the boot order from ide2 (the iso) to scsi0 (the disk)
+    # For this to work properly, shutoff after install has to be enabled in cloud-config (user-data)
+    while true; do
+        VM_STATE=$(qm status $ID | awk '{print $2}')
+        if [ "$VM_STATE" == "stopped" ]; then
+            echo "Installation complete. Changing boot order..."
+            qm set $ID --boot order=scsi0
+            echo "Boot order changed. Starting VM..."
+            qm start $ID
+            echo "VM started. Done!"
+            break
+        fi
+        echo "Waiting for installation to complete..."
+        sleep 30
+    done
+
 }
 
 if [ $# -eq 0 ]; then
